289911a7a1c0b0b3ccc6fa3a00af47103927f7e1
frkgui <78104494+frkgui@users.noreply.github.com>
2023-05-26 10:50:38 -0300

    Update main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("--- Sistema de Atendimento ---");
+        System.out.println("""
+                Serviços disponíveis:
+                \n
+                () -
+                () -
+                () -
+                () -
+                () -
+                () -
+                () -
+                () -
+                """);
+        System.out.print("Digite a opção desejada: ");
 
 
-
+        input.close();
     }
 
 }
